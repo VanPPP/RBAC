df73aedc8482e07b1a4579978cade6a66870fd45
VanPPP <ivan.proskokov@gmail.com>
2026-02-16 23:58:12 +0700

    1.7: Add PermanentAssignment

---
+++ src/main/java/com/rbac/model/PermanentAssignment.java
@@ -0,0 +1,27 @@
+package com.rbac.model;
+
+public class PermanentAssignment extends AbstractRoleAssignment {
+    private boolean revoked = false;
+
+    public PermanentAssignment(User user, Role role, AssignmentMetadata metadata) {
+        super(user, role, metadata);
+    }
+
+    @Override
+    public boolean isActive() {
+        return !revoked;
+    }
+
+    @Override
+    public String assignmentType() {
+        return "PERMANENT";
+    }
+
+    public void revoke() {
+        this.revoked = true;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+}
